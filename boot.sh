61e7c0fad56ac8638efd78ddfe276067233c0307
quantscientist <arya.has.fun@gmail.com>
2021-08-27 17:48:35 -0700

    added nginx container

--- boot.sh
+++ boot.sh
@@ -25,5 +25,8 @@ while true; do
     sleep 5
 done
 
-exec gunicorn -b :5000 --access-logfile - --error-logfile - yowla:app --timeout 120
+exec gunicorn -w 1 -b :5000 yowla:app
+#exec gunicorn -b :5000 --access-logfile - --error-logfile - yowla:app --timeout 120
+#exec gunicorn --workers 3 --bind unix:myproject.sock -m 000 --access-logfile - --error-logfile - yowla:app --timeout 120 >> /var/log/yowla.sys.log 2>&1
 
+echo "EXECUTED GUNICORN!!"
